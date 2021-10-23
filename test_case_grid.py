938e0908624af83b393a9c6077a12fe2af20f36c
Peteris-Zvejnieks <53169859+Peteris-Zvejnieks@users.noreply.github.com>
2021-10-23 17:43:21 +0200

    Update test_case_grid.py

--- test_case_grid.py
+++ test_case_grid.py
@@ -7,6 +7,13 @@ mpl.rcParams['figure.dpi'] = 500
 
 from DivergenceFreeInterpolant import interpolant
 #%%
+from datetime import datetime
+import shutil
+
+now = datetime.now()
+string = now.strftime("%d:%m:%Y  %H:%M:%S")
+path = './.test_plots/'+string
+
 v_f = lambda x, y: np.array([-2*x**3 * y, 3*x**2 * y**2])
 xmin, xmax = 0, 1
 ymin, ymax = 0, 1
@@ -22,23 +29,28 @@ plt.quiver(XX, YY, UV[:, :,0], UV[:,:,1], S)
 ax = plt.gca()
 plt.colorbar()
 ax.set_aspect('equal')
-plt.show()
-plt.close()
 #%%
 
 interp = interpolant(5, 3, 2.5)
 #%%
 t1 = time.perf_counter()
 
-interp.condition(np.array([XX.flatten(), YY.flatten()]).T, UV.reshape(l1*l2,2))
-
+interp.condition(np.array([XX.flatten(), YY.flatten()]).T, UV.reshape(l1*l2,2), 3)
 print('Conditioning: ', time.perf_counter() - t1)
+os.mkdir(path)
+plt.savefig(path + '/0_sample_points.png')
+plt.close()
+
 #%%
 ll1, ll2 = 100, 100
 crdsX, crdsY = np.mgrid[xmin:xmax:ll1*1j, ymin:ymax:ll2*1j]
 
 t1 = time.perf_counter()
-uv = interp(crdsX, crdsY)
+try:
+    uv = interp(crdsX, crdsY)
+except:
+    shutil.rmtree(path)
+    raise BrokenPipeError("Interpolating")
 print('Time per interpolation: ', (time.perf_counter() - t1)/(ll1*ll2))
 
 SS = (uv[:,:,0]**2 + uv[:,:,1]**2)**0.5
@@ -50,15 +62,39 @@ ax = fig.add_subplot(111)
 stream = ax.streamplot(crdsX.T, crdsY.T, uv[:, :, 0].T, uv[:, :, 1].T, color = SS.T, density = 1, cmap ='autumn')
 fig.colorbar(stream.lines)
 ax.set_aspect('equal')
-plt.show()
+plt.savefig(path + '/1_interpolation.png')
 plt.close()
 #%%
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
 
-arrows = ax.quiver(crdsX, crdsY, uv[:,:,0]/SS, uv[:,:,1]/SS, SS)
-fig.colorbar(arrows)
+plt.plot(interp.support_radii_history, '-o')
+plt.title('support radii')
+plt.savefig(path + '/2_support_radii.png')
+plt.close()
+
+plt.plot(np.abs(interp.mistako_history))
+plt.title('|mistakos|')
+plt.yscale('log')
+plt.ylabel('log|mistakos|')
+
+plt.savefig(path + '/3_mistakos.png')
+plt.close()
+
+
+plt.plot(interp.condition_numbers)
+plt.title('condition_number')
+plt.yscale('log')
+plt.ylabel('log(Cond(A))')
+
+plt.savefig(path + '/4_condition_number.png')
+plt.close()
+
+norm = (interp.residuals[:,0]**2 + interp.residuals[:,1]**2)**0.5
+arrows = plt.quiver(interp.XY[:,0], interp.XY[:,1], interp.residuals[:,0]/norm, interp.residuals[:,1]/norm, norm)
+plt.title('Residuals')
+plt.colorbar(arrows)
+ax = plt.gca()
 ax.set_aspect('equal')
-plt.show()
+plt.savefig(path + '/5_residuals.png')
+plt.close()
 plt.close()
